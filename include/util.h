84766b428b3fc116287557204b2c2e3e713bb5b7
Gedas <gediminas.repecka@gmail.com>
2023-07-04 19:31:10 +0300

    Revert "added ability to verify event signatures"
    This reverts commit 11af45c8650de5c0c368d40d00145197fcfe2445.

--- include/util.h
+++ include/util.h
@@ -4,4 +4,3 @@
 #include <stdio.h>
 
 void base64_encode(const unsigned char *input, size_t input_length, char *output);
-int hex_to_bin(const char* hex_string, unsigned char* bin_data, size_t bin_size);
